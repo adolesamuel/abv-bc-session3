0e0427921fad7096a4ebdc8c36c64477dc74d6e3
Adole Samuel <adolesamuel4@gmail.com>
2017-05-18 09:11:15 +0100

    Final commit of Findmissing function

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # abv-bc-session3
-## Question 1: reverseString
+## Question 2: findMissing
 #### Run "npm install jasmine" to install jasmine test module
-#### Run "npm test" or "jasmine" to carry out tests
-#### 
+#### Run "npm test" or "jasmine" to carry out tests in "../spec/spec.js"
--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 'use strict';
 
 module.exports = {
-	reverseString: (text) =>{
-		if(text.length === 0){
-			return null;
-		}
-		let arr = [];
-		for (let i = text.length-1; i>=0; --i){
-		//loop counts from right to left of string the array.
-			arr += text[i];
-		}
-		let str = arr.toString();
-		if (str === text){
-			return true;
-		}
-		else return str;
+	findMissing: (arr1,arr2) =>{
+	let bigArray = [];
+  	let smallArray = [];
+  	//if either array is emptry 
+  	if (arr1.length === 0 || arr2.length === 0) return 0;
+  	// if arrays are the same
+  	else if (arr1.length==arr2.length) return 0;
+  	//arranges array properly
+  	else if (arr1.length > arr2.length){
+    	bigArray = arr1;
+    	smallArray = arr2;
+  	}
+  	else{
+    	bigArray=arr2;
+    	smallArray=arr1;
+  	}
+  	//checks if each bigArray element can be found in smallArray
+  for (let i = 0 ; i<bigArray.length;i++){
+    let position = smallArray.indexOf(bigArray[i]);
+    if (position === -1) return bigArray[i];
+    }
 	}
 }
